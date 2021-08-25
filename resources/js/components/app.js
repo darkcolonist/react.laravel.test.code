f7d7696069b7d13e0be6b378eeb93459c733d3ee
cris <darkcolonist@gmail.com>
2021-08-25 14:07:46 +0800

    #3 material ui implementation still working with routes and button links

--- resources/js/components/app.js
+++ resources/js/components/app.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import { React, Component } from 'react';
+import { BrowserRouter, Link, useHistory } from "react-router-dom";
 import { Button } from '@material-ui/core';
 import { makeStyles, createTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-
 function App() {
   const darkTheme = createTheme({
     palette: {
@@ -24,39 +24,48 @@ function App() {
   }));
 
   const classes = useStyles();
-
+  
+  let routeChange = () => {
+    let path = `newPath`;
+    let history = useHistory();
+    history.push(path);
+  }
+  
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       {/* <Routes */}
-      <Button>A Button</Button>
-      <Button to="/another">Another Button</Button>
+      <BrowserRouter>
+        <Link to="/home">Home</Link>
+        <Link to="/somewhere">clickme</Link>
+        <Link to="/somewhere">clickme</Link>
+      </BrowserRouter>
+      <Button onClick={routeChange}>Home</Button>
+      <Button>About</Button>
 
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>xs=12</Paper>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>xs=6</Paper>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>xs=6</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
         </Grid>
-      </div>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+      </Grid>
 
     </ThemeProvider>
   );
