b5848e3117a812a6f42b09a03f2cbc6d062206f3
christian <darkcolonist@gmail.com>
2021-09-21 17:06:35 +0800

    #7 administration/modals hitting the edit button in datatable will open modal TODO move modal into separate component for re-usability

--- resources/js/components/admin/DataTableOptionButtons.js
+++ resources/js/components/admin/DataTableOptionButtons.js
@@ -1,19 +1,57 @@
-import { Button, ButtonGroup, IconButton } from "@material-ui/core";
+import { Box, IconButton, Modal, Typography } from "@material-ui/core";
 import { Edit, Delete } from "@material-ui/icons";
 import { Component } from "react";
 
 class DataTableOptionButtons extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modalOpen: false
+    }
+
+    this.style = {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: 400,
+      bgcolor: 'background.paper',
+      border: '2px solid #000',
+      boxShadow: 24,
+      p: 4,
+    };
+  }
+
+  handleEditClick = () => {
+    this.setState({ modalOpen: true })
+  }
+
+  handleClose = () => {
+    this.setState({ modalOpen: false })
   }
 
   render() {
     return (
       <div class="DataTableOptionButtons">
-        <IconButton title="edit"><Edit /></IconButton>
+        <IconButton title="edit" onClick={this.handleEditClick}><Edit /></IconButton>
         <IconButton title="delete" onClick={() => {
           window.alert("attempting to delete "+this.props.theData.rowData[0]);
           }}><Delete /></IconButton>
+        <Modal
+          open={this.state.modalOpen}
+          onClose={this.handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={this.style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
       </div>
     );
   }
