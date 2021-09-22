f4f67d84358faceb7ac75e9bff53e9842ac13788
christian <darkcolonist@gmail.com>
2021-09-22 12:18:14 +0800

    #7 administration/users added database implementation and seeder to start working with persistence via API

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $users = [
+        [ "hash" => Str::random(rand(24,32)), "first_name" => "Cecelia", "last_name" => "Marshall", 
+          "email" => "anerul@ce.ky", "password" => "null_for_now" ],
+        [ "hash" => Str::random(rand(24,32)), "first_name" => "Tyler", "last_name" => "Ingram", 
+          "email" => "enevale@va.ge", "password" => "null_for_now" ],
+        [ "hash" => Str::random(rand(24,32)), "first_name" => "Lela", "last_name" => "Hodges", 
+          "email" => "picuup@welu.gr", "password" => "null_for_now" ],
+        [ "hash" => Str::random(rand(24,32)), "first_name" => "Roy", "last_name" => "Holt", 
+          "email" => "wucnispe@jinvavaku.la", "password" => "null_for_now" ],
+        [ "hash" => Str::random(rand(24,32)), "first_name" => "Gregory", "last_name" => "Ramos", 
+          "email" => "suhwi@hinjukneg.bb", "password" => "null_for_now" ],
+        [ "hash" => Str::random(rand(24,32)), "first_name" => "Lizzie", "last_name" => "Alvarado", 
+          "email" => "evresut@afbiasa.ad", "password" => "null_for_now"]
+      ];
+
+      $result = DB::table('users')->insert($users);
+
+
+    }
+}
