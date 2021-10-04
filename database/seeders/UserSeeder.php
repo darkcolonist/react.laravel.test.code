3f043bea836c734efafd46691018a9a7e931269a
christian <darkcolonist@gmail.com>
2021-10-04 16:18:15 +0800

    #7 administration/users testing for hundreds of data TODO lazy loading TODO new/add TODO delete

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,20 +15,34 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-      $users = [
-        [ "hash" => Str::random(rand(24,32)), "first_name" => "Cecelia", "last_name" => "Marshall", 
-          "email" => "anerul@ce.ky", "password" => "null_for_now" ],
-        [ "hash" => Str::random(rand(24,32)), "first_name" => "Tyler", "last_name" => "Ingram", 
-          "email" => "enevale@va.ge", "password" => "null_for_now" ],
-        [ "hash" => Str::random(rand(24,32)), "first_name" => "Lela", "last_name" => "Hodges", 
-          "email" => "picuup@welu.gr", "password" => "null_for_now" ],
-        [ "hash" => Str::random(rand(24,32)), "first_name" => "Roy", "last_name" => "Holt", 
-          "email" => "wucnispe@jinvavaku.la", "password" => "null_for_now" ],
-        [ "hash" => Str::random(rand(24,32)), "first_name" => "Gregory", "last_name" => "Ramos", 
-          "email" => "suhwi@hinjukneg.bb", "password" => "null_for_now" ],
-        [ "hash" => Str::random(rand(24,32)), "first_name" => "Lizzie", "last_name" => "Alvarado", 
-          "email" => "evresut@afbiasa.ad", "password" => "null_for_now"]
-      ];
+      $howMany = 100;
+      $users = [];
+
+      for ($i=0; $i < $howMany; $i++) { 
+        $faker = \Faker\Factory::create();
+        $users[] = [
+          "hash" => Str::random(rand(24,32)),
+          "first_name" => $faker->name(),
+          "last_name" => $faker->name(),
+          "email" => $faker->safeEmail,
+          "password" => "NULLFORNOW",
+        ];
+      }
+
+      // $users = [
+      //   [ "hash" => Str::random(rand(24,32)), "first_name" => "Cecelia", "last_name" => "Marshall", 
+      //     "email" => "anerul@ce.ky", "password" => "null_for_now" ],
+      //   [ "hash" => Str::random(rand(24,32)), "first_name" => "Tyler", "last_name" => "Ingram", 
+      //     "email" => "enevale@va.ge", "password" => "null_for_now" ],
+      //   [ "hash" => Str::random(rand(24,32)), "first_name" => "Lela", "last_name" => "Hodges", 
+      //     "email" => "picuup@welu.gr", "password" => "null_for_now" ],
+      //   [ "hash" => Str::random(rand(24,32)), "first_name" => "Roy", "last_name" => "Holt", 
+      //     "email" => "wucnispe@jinvavaku.la", "password" => "null_for_now" ],
+      //   [ "hash" => Str::random(rand(24,32)), "first_name" => "Gregory", "last_name" => "Ramos", 
+      //     "email" => "suhwi@hinjukneg.bb", "password" => "null_for_now" ],
+      //   [ "hash" => Str::random(rand(24,32)), "first_name" => "Lizzie", "last_name" => "Alvarado", 
+      //     "email" => "evresut@afbiasa.ad", "password" => "null_for_now"]
+      // ];
 
       $result = DB::table('users')->insert($users);
 
